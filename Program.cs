84f2de72daa29d8617db62bd28aef7e4fc423e57
Lioxhin <liosantiveladiazxd@gmail.com>
2026-05-26 09:37:48 -0500

    Hice el ingreso de datos y mostré las notas

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int cantidad;
+
+        Console.Write("¿Cuántas notas deseas ingresar? ");
+        cantidad = int.Parse(Console.ReadLine());
+
+        int[] notas = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write("Ingresa la nota " + (i + 1) + ": ");
+            notas[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nLas notas ingresadas son:");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.WriteLine("Nota " + (i + 1) + ": " + notas[i]);
+        }
+    }
+}
